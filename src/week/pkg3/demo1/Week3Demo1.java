99caf54159f4948a2848a1dc235165e82b2ccd94
Vidhi Patel <patevidh@sheridancollege.ca>
2024-09-16 12:49:50 -0400

    initial commit I have add the comment

---
+++ src/week/pkg3/demo1/Week3Demo1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week.pkg3.demo1;
+
+/**
+ *
+ * @author vidhi patel
+ * @cite some thing over here
+ */
+public class Week3Demo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //added a comment 
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // TODO code application logic here
+    }
+    
+}
